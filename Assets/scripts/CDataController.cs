a4e2c950a20934d0cd4c1169879b3433dc13f096
lacey0905 <lacey0905@naver.com>
2019-04-18 18:42:16 +0900

    클리커 튜토리얼 시작

---
+++ Assets/scripts/CDataController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CDataController : MonoBehaviour
+{
+    [SerializeField]
+    private int m_gold = 0;
+
+    [SerializeField]
+    private int m_goldPerClick = 0;
+
+    void Awake() {
+        // Local 데이터 저장
+        // Key : Velue
+        m_gold = PlayerPrefs.GetInt("Gold");
+        m_goldPerClick = PlayerPrefs.GetInt("GoldPerClick", 1);
+    }
+
+    public void SetGold(int newGold)
+    {
+        m_gold = newGold;
+        PlayerPrefs.SetInt("Gold", m_gold);
+    }
+
+    public void AddGold(int newGold)
+    {
+        m_gold += newGold;
+        SetGold(m_gold);
+    }
+
+    public void SubGold(int newGold)
+    {
+        m_gold -= newGold;
+        SetGold(m_gold);
+    }
+
+    public int GetGold()
+    {
+        return m_gold;
+    }
+
+    public int GetGoldPerClick()
+    {
+        return m_goldPerClick;
+    }
+
+    public void SetGoldPerClick(int newGoldPerClick)
+    {
+        m_goldPerClick = newGoldPerClick;
+        PlayerPrefs.SetInt("GoldPerClick", m_goldPerClick);
+    }
+
+}
---
+++ Assets/scripts/CUIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CUIManager : MonoBehaviour
+{
+    public Text goldDisplayer;
+    public CDataController dataController;
+
+    void Update() 
+    {
+        goldDisplayer.text = "GOLD : " + dataController.GetGold();
+    }
+
+}
